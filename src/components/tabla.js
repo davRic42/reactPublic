f4054a27c1986e180f6cd29f39230d358c11c481
davRic42 <davidric.2710@gmail.com>
2024-02-13 09:35:55 -0500

    avance tabla

--- src/components/tabla.js
+++ src/components/tabla.js
@@ -4,65 +4,69 @@ import ButtonUp from "./btnupd";
 import axios from "axios";
 
 export function Rows() {
-  const [data, setData] = useState([]);
-  const [shouldUpdate, setShouldUpdate] = useState(false);
+    const [data, setData] = useState([]);
+    const [shouldUpdate, setShouldUpdate] = useState(false);
+  
+    const fetchData = async () => {
+        try {
+            const response = await axios.get("/user");
 
-  const AllUser = async () => {
-    try {
-      const row = await axios.get("/user");
-      console.log(row.data);
-      setData(row.data);
-    } catch (error) {
-        console.log(error)
-    }
-  };
+            setData(response.data);
 
-  useEffect(() => {
-    if (data) {
-      AllUser();
-      setShouldUpdate(false)
-    }
-  }, [shouldUpdate]);
+        } catch (error) {
+            console.error("Error al obtener los datos:", error);
+        }
+    };
 
-  return (
-    <>
-      {data.map((datos) => {
-        <tr>
-          <td>{datos.userId}</td>
-          <td>{datos.userName}</td>
-          <td>{datos.userPhone}</td>
-          <td>
-            <ButtonDel userId={datos.userId} />
-            <br />
-            <br />
-            <ButtonUp userId={datos.userId} />
-          </td>
-        </tr>;
-      })}
-    </>
-  );
+    useEffect(() => {
+
+        fetchData();
+        setShouldUpdate(false);
+
+
+
+    }, [shouldUpdate]);
+    console.log(data);
+    return (
+        <>
+        
+            {data.map((datos) => 
+                <tr >
+                    <td>{datos.userId}</td>
+                    <td>{datos.userName}</td>
+                    <td>{datos.userPhone}</td>
+                    <td>
+                        <ButtonDel userId={datos.userId} />
+                        <br />
+                        <br />
+                        <ButtonUp userId={datos} />
+                    </td>
+                </tr>
+            )}
+        </>
+    );
 }
 
 export default function Table() {
-  // Filtrar los usuarios con stateRow igual a 1
-  //const filteredUsers = rows.filter((user) => user.stateRow === true);
+    // Filtrar los usuarios con stateRow igual a 1
+    //const filteredUsers = rows.filter((user) => user.stateRow === true);
 
-  return (
-    <div className="container-fluid p-4  mt-8 w-70">
-      <table className="table table-hover">
-        <thead className="table-info">
-          <tr>
-            <th scope="col">id</th>
-            <th scope="col">nombre</th>
-            <th scope="col">telefono celular</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody className="table-secondary">
-            <Rows
-            />
-        </tbody>
-      </table>
-    </div>
-  );
+    return (
+        <div className="container-fluid p-4  mt-8 w-70">
+            <table className="table table-hover">
+                <thead className="table-info">
+                    <tr>
+                        <th scope="col">id</th>
+                        <th scope="col">nombre</th>
+                        <th scope="col">telefono celular</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody className="table-secondary">
+                    <Rows
+                    />
+                </tbody>
+            </table>
+        </div>
+    );
 }
